c274e254bb214fd787374616f673c6279903f58c
Amine Elsayed <aelsayed@c3r10p5.1337.ma>
2026-03-02 09:53:11 +0000

    setting up projects up

--- BACKEND/projects/views.py
+++ BACKEND/projects/views.py
@@ -3,8 +3,14 @@ from django.http import HttpResponse
 # Create your views here.
 # a view function is a func that takes a request and returns a response (request handler) 
 
+def calculate():
+    x = 1
+    y = 2
+    return x
+
 def do_something(request):
     # we usually can pull from database
     # transform data
     # send somthing (emails, msgs...)
-    return HttpResponse("hello world")
+    calculate()
+    return render(request, 'projects.html', {'name': 'proj1'})
